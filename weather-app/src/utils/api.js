7c500b9505b77a3aa2f95c0be17a64774b524092
BenasUrba <benasurba@gmail.com>
2024-11-03 19:12:35 +0200

    Safety Features Added

--- weather-app/src/utils/api.js
+++ weather-app/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_KEY = 'd329e9dec65864b6887bca924d52f42b';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchWeatherData = async (city) => {
